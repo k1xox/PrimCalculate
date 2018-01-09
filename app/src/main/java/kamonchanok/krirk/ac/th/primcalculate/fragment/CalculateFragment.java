2f3db7d678bf0734d98cacefc1b724269bd92567
kamonchanok <primkamonchanok@gmail.com>
2018-01-09 15:44:22 +0700

    Back Controller

--- app/src/main/java/kamonchanok/krirk/ac/th/primcalculate/fragment/CalculateFragment.java
+++ app/src/main/java/kamonchanok/krirk/ac/th/primcalculate/fragment/CalculateFragment.java
@@ -4,10 +4,12 @@ package kamonchanok.krirk.ac.th.primcalculate.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import kamonchanok.krirk.ac.th.primcalculate.MainActivity;
 import kamonchanok.krirk.ac.th.primcalculate.R;
 
 /**
@@ -15,6 +17,37 @@ import kamonchanok.krirk.ac.th.primcalculate.R;
  */
 
 public class CalculateFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        //Create Toolbar
+        createToolbar();
+
+
+    } //Main Method
+
+    private void createToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarCalculate);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setTitle("Calculate");
+
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar()
+                .setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
