d00195e3d81318abd36d0445a352739cce72fa13
kamonchanok <primkamonchanok@gmail.com>
2018-01-09 11:36:18 +0700

    Initial commit

---
+++ app/src/main/java/kamonchanok/krirk/ac/th/primcalculate/fragment/MainFragment.java
@@ -0,0 +1,40 @@
+package kamonchanok.krirk.ac.th.primcalculate.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import kamonchanok.krirk.ac.th.primcalculate.MainActivity;
+import kamonchanok.krirk.ac.th.primcalculate.R;
+
+/**
+ * Created by User on 9/1/2561.
+ */
+
+public class MainFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //Create Toolbar
+        createToolbar();
+
+    } //Main Method
+
+    private void createToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.ToolbarMain);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main,container,false);
+        return view;
+    }
+} //Main Class
